7532b50db1a334894bc6407874c3b0d1d9255dc4
Alexander Tetervak <tetervak@sheridancollege.ca>
2023-01-11 14:48:07 -0500

    added Die class

--- src/main/java/ca/tetervak/Main.java
+++ src/main/java/ca/tetervak/Main.java
@@ -1,7 +1,26 @@
 package ca.tetervak;
 
+import ca.tetervak.model.Die;
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        out.println("Hello from MyDice App!");
+
+        Die die = new Die();
+        out.println("Initial die value: " + die.getValue());
+
+        out.print("Die rolling values:");
+        for (int i = 1; i <= 5; i++) {
+            die.roll();
+            out.print(" " + die.getValue());
+        }
+        out.println();
+
+        die.reset();
+        out.println("Die reset value: " + die.getValue());
+
+        out.println("See you later!");
     }
 }
